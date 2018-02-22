9f30f24d81c6e094ebb53c10d2ad2cd7ee8b17bd
KarenChincoya <30363201+KarenChincoya@users.noreply.github.com>
2018-02-22 08:01:00 -0600

    Update Monomio.java

--- src/testpolinomios/Monomio.java
+++ src/testpolinomios/Monomio.java
@@ -10,5 +10,45 @@ package testpolinomios;
  * @author Karen Velasco
  */
 public class Monomio {
+    private Integer coeficiente;
+    private Integer exponente;
+    
+    private Monomio next;
+    
+    public Monomio(){
+        this.coeficiente = 0;
+        this.exponente = 0;
+    }
+    
+    public Monomio(Integer coeficiente, Integer exponente){
+        this.coeficiente = coeficiente;
+        this.exponente = exponente;
+    }   
+    
+    //getters and setters
+    
+    public void setCoeficiente(Integer coeficiente){
+        this.coeficiente = coeficiente;
+    }
+    
+    public Integer getCoeficiente(){
+        return coeficiente;
+    }
+    
+    public void setExponente(Integer exponente){
+        this.exponente = exponente;
+    }
+    
+    public Integer getExponente(){
+        return exponente;
+    }
+    
+    public void setNext(Monomio next){
+        this.next = next;
+    }
+    
+    public Monomio getNext(){
+        return next;
+    }
     
 }
